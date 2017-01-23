2cac373cfde12e6d7e97a176a68733a38be33fac
pbzweihander <sd852456@naver.com>
2017-01-23 22:45:42 +0900

    two line

--- install_chips.sh
+++ install_chips.sh
@@ -1,4 +1,3 @@
 curl -Lo ~/utils/chips --create-dirs \
-    https://github.com/xtendo-org/chips/releases/download/1.1.2/chips_gnulinux \
-    ; and chmod +x ~/utils/chips
-~/utils/chips
+    https://github.com/xtendo-org/chips/releases/download/1.1.2/chips_gnulinux
+chmod +x ~/utils/chips
