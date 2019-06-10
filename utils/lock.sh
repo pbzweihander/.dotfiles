0f7aafbc81eaf28eba01cebfa13c511ee3b90162
rusty <rusty@spoqa.com>
2019-06-10 17:39:51 +0900

    utils: change && into newline

--- utils/lock.sh
+++ utils/lock.sh
@@ -1,4 +1,5 @@
 #!/bin/sh -e
 scrot /tmp/lock-screen.png
-convert /tmp/lock-screen.png -scale 5% -scale 2000% /tmp/lock-screen.png && i3lock -e -i /tmp/lock-screen.png -c 000000
+convert /tmp/lock-screen.png -scale 5% -scale 2000% /tmp/lock-screen.png
+i3lock -e -i /tmp/lock-screen.png -c 000000
 sleep 1
