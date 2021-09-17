51e92f174a29d0325b7cddb8e8bcc50b98c1c0c6
Kangwook Lee <pbzweihander@gmail.com>
2021-09-17 18:23:19 +0900

    Update sway service

--- utils/sway-service.sh
+++ utils/sway-service.sh
@@ -1,5 +1,7 @@
 #! /bin/sh
 
+export $(/usr/lib/systemd/user-environment-generators/30-systemd-environment-d-generator)
+
 # first import environment variables from the login manager
 systemctl --user import-environment
 # then start the service
