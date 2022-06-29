9233e23475862711db2a25da6da6514d2ec455e2
pbzweihander <pbzweihander@gmail.com>
2022-06-29 16:40:35 +0900

    utils: Parallelize lock

--- utils/lock.sh
+++ utils/lock.sh
@@ -1,11 +1,20 @@
 #!/usr/bin/bash -e
-for OUTPUT in $(swaymsg -rt get_outputs | jq -r .[].name); do \
-    grim -o $OUTPUT /tmp/lock-screen-${OUTPUT}.png &&\
-    convert \
-        /tmp/lock-screen-${OUTPUT}.png \
-        -scale 2.5% \
-        -scale 4000% \
-        /tmp/lock-screen-${OUTPUT}.png &&\
-    echo -i ${OUTPUT}:/tmp/lock-screen-${OUTPUT}.png; done |\
-    xargs swaylock -fc 000000 &&\
-    rm -f /tmp/lock-screen-*.png
+OUTPUTS=$(swaymsg -rt get_outputs | jq -r '.[].name')
+for OUTPUT in $OUTPUTS; do
+    (
+        grim -o "$OUTPUT" "/tmp/lock-screen-${OUTPUT}.png" &&\
+        convert \
+            "/tmp/lock-screen-${OUTPUT}.png" \
+            -scale 2.5% \
+            -scale 4000% \
+            "/tmp/lock-screen-${OUTPUT}.png"
+    ) &
+done
+wait
+for OUTPUT in $OUTPUTS; do
+    echo "-i ${OUTPUT}:/tmp/lock-screen-${OUTPUT}.png"
+done |\
+    xargs swaylock -fc 000000
+for OUTPUT in $OUTPUTS; do
+    rm -f "/tmp/lock-screen-${OUTPUT}.png"
+done
