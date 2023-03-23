0bbc59bb1e977eebe88aa93c1508aacc00de1e9c
pbzweihander <pbzweihander@gmail.com>
2023-03-23 22:38:02 +0900

    acpid: Update tablet mode script

--- etc/acpi/actions/tablet-mode.sh
+++ etc/acpi/actions/tablet-mode.sh
@@ -13,5 +13,6 @@ case "$4" in
   00000000)
     SWAYSOCK="/run/user/${USER_ID}/sway-ipc.${USER_ID}.${SWAY_PID}.sock" su "${USER}" -c "swaymsg input 1386:21125:Wacom_HID_5285_Finger events disabled"
     killall rot8
+    SWAYSOCK="/run/user/${USER_ID}/sway-ipc.${USER_ID}.${SWAY_PID}.sock" su "${USER}" -c "swaymsg output eDP-1 transform 0"
     ;;
 esac
