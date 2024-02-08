44e7d5b452521ff8da5da935df80486659024f02
pbzweihander <pbzweihander@gmail.com>
2024-02-08 19:41:52 +0900

    Update

--- home/.config/fish/config.fish
+++ home/.config/fish/config.fish
@@ -1,10 +1,11 @@
 # disable greet message
 set -gx fish_greeting
 
-if status --is-login
+if status is-login
     # set up environment variables
     export $(/usr/lib/systemd/user-environment-generators/30-systemd-environment-d-generator)
-else if status is-interactive
+end
+if status is-interactive
     # interactive configs
 
     if type -q starship
